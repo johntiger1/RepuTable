3279cfa7d86612f0fdcf8684505c056f842552eb
johntiger1 <john.person1@gmail.com>
2018-04-22 10:19:58 -0400

    got the comms working! possibly was an issue with the sendBadge since it has no return

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,26 +1,17 @@
 
-
+//OK, Remix and WEB3 does actually require an Internet connection!
 function fetchResults()
 {
 
-    RepToken.sendRepBadge.call(0x64319CA297239d8652A0b5f0F12dD6666cB0e05b, "live from the other code",
-
-        function(error, result)
+    RepToken.badges(0x64319CA297239d8652A0b5f0F12dD6666cB0e05b, function(error, result){
+        if(!error)
         {
-            console.log("done!");
+            $("#badge_result").html(result[0]+' ('+result[1]+' years old)' + result[2] + result[3]);
             console.log(result);
-            // RepToken.badges(document.getElementById("uaddress").innerText,function(error, result){
-            //     if(!error)
-            //     {
-            //         $("#badge_result").html(result[0]+' ('+result[1]+' years old)' + result[2] + result[3]);
-            //         console.log(result);
-            //     }
-            //     else
-            //         console.error(error);
-            // });
         }
-
-        );
+        else
+            console.error(error);
+    });
 
     //might also be attaching the callback incorrectly...
     // console.log(document.getElementById("uaddress").innerText);
