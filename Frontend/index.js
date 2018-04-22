4966c7f54a54ba272c29af56f209dce8e44a7117
johntiger1 <john.person1@gmail.com>
2018-04-22 10:17:35 -0400

    extent of debugging

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,20 +3,21 @@
 function fetchResults()
 {
 
-    RepToken.sendRepBadge(0x64319CA297239d8652A0b5f0F12dD6666cB0e05b, "sd",
+    RepToken.sendRepBadge.call(0x64319CA297239d8652A0b5f0F12dD6666cB0e05b, "live from the other code",
 
         function(error, result)
         {
-
-            RepToken.badges(document.getElementById("uaddress").innerText,function(error, result){
-                if(!error)
-                {
-                    $("#badge_result").html(result[0]+' ('+result[1]+' years old)' + result[2] + result[3]);
-                    console.log(result);
-                }
-                else
-                    console.error(error);
-            });
+            console.log("done!");
+            console.log(result);
+            // RepToken.badges(document.getElementById("uaddress").innerText,function(error, result){
+            //     if(!error)
+            //     {
+            //         $("#badge_result").html(result[0]+' ('+result[1]+' years old)' + result[2] + result[3]);
+            //         console.log(result);
+            //     }
+            //     else
+            //         console.error(error);
+            // });
         }
 
         );
